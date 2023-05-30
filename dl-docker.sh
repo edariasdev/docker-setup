2622376264fce2e1c2d4f1f0ce8ac6f8a1c3b01b
Ed <22520564+mannydr3@users.noreply.github.com>
2023-05-29 21:20:00 -0400

    Update dl-docker.sh

--- dl-docker.sh
+++ dl-docker.sh
@@ -2,20 +2,21 @@
 
 codeName=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f 2)
 
-#Installing Docker
+# Installing Docker
 echo "Installing Docker"
+export DEBIAN_FRONTEND=noninteractive
 apt update -y &&\
 apt install -y apt-transport-https ca-certificates curl software-properties-common &&\
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&\
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${codeName} stable" &&\
 apt update -y &&\
-apt install docker-ce -y &&\
-usermod -aG docker $(echo $SUDO_USER) &&\
+apt install -y docker-ce &&\
+usermod -aG docker "$(echo "$SUDO_USER")" &&\
 echo "Done!"
 
-
-#Installing Compose
+# Installing Compose
 echo "Installing Compose"
 curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&\
 chmod +x /usr/local/bin/docker-compose
 echo "Done!"
+
