071a7501b149a577569eb2aa4486c9fcac5c95b4
Ed <22520564+mannydr3@users.noreply.github.com>
2023-05-29 21:12:48 -0400

    Update dl-docker.sh

--- dl-docker.sh
+++ dl-docker.sh
@@ -4,12 +4,12 @@ codeName=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f 2)
 
 #Installing Docker
 echo "Installing Docker"
-apt update &&\
+apt update -y &&\
 apt install apt-transport-https ca-certificates curl software-properties-common &&\
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&\
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${codeName} stable" &&\
-apt update
-apt install docker-ce &&\
+apt update -y
+apt install docker-ce -y &&\
 usermod -aG docker $(echo $SUDO_USER) &&\
 echo "Done!"
 
